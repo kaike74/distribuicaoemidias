a3c73b0103f6417412e592d71d10bd2222127e78
kaike74 <kaike@hubradios.com>
2025-10-13 17:54:27 -0300

    Update notion.js

--- functions/notion.js
+++ functions/notion.js
@@ -32,7 +32,7 @@ export async function onRequest(context) {
     }
 
     // ===== BUSCAR TOKEN NOTION_TOKEN =====
-    const notionToken = env.Notion_Token;
+    const notionToken = env.NOTION_TOKEN;
     
     console.log('=== DEBUG CLOUDFLARE ===');
     console.log('1. Token existe?', !!notionToken);
